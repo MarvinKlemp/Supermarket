a5ef9fd0262852eba7c46cd6f23690ba1c51becb
MarvinKlemp <itklemp@googlemail.com>
2015-02-23 17:52:59 +0000

    initial commit

---
+++ SuperMarket/src/Product.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace CodingKatas\SuperMarket;
+
+class Product
+{
+
+} 
---
+++ SuperMarket/tests/ProductTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CodingKatas\SuperMarket\Tests;
+
+use CodingKatas\SuperMarket\Product;
+
+class ProductTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_init()
+    {
+        $product = new Product();
+    }
+} 
