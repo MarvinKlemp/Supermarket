ab7c059458b1f92bfb99c6aa8014c9a40a80e6bd
MarvinKlemp <itklemp@googlemail.com>
2015-04-21 19:03:36 +0000

    Added simple shopping bag

--- src/ShoppingBag.php
+++ src/ShoppingBag.php
@@ -5,10 +5,31 @@ namespace MarvinKlemp\SuperMarket;
 class ShoppingBag implements BuyableInterface
 {
     /**
+     * @var BuyableInterface[]
+     */
+    protected $items;
+
+    public function __construct()
+    {
+        $this->items = [];
+    }
+
+    /**
      * @return double
      */
     public function costs()
     {
-        // TODO: Implement costs() method.
+        $res = 0;
+
+        foreach ($this->items as $item) {
+            $res += $item->costs();
+        }
+
+        return $res;
+    }
+
+    public function put(BuyableInterface $item)
+    {
+        $this->items[] = $item;
     }
 }
---
+++ tests/ShoppingBagTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MarvinKlemp\SuperMarket\tests;
+
+
+class ShoppingBagTest extends \PHPUnit_Framework_TestCase
+{
+}
